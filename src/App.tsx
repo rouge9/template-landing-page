748ea2cb863a7f84782f5e055ef9542469a59b06
rouge9 <robelged1992@gmail.com>
2024-03-21 21:55:50 -0900

    Add NavBar and Footer components to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { Fragment } from "react/jsx-runtime";
 import Collection from "./components/Sections/Collection";
 import Feature from "./components/Sections/Feature";
 import Hero from "./components/Sections/Hero";
-// import NavBar from "./components/Shared/NavBar";
+import NavBar from "./components/Shared/NavBar";
+import Footer from "./components/Shared/Footer";
 
 function App() {
   return (
     <Fragment>
-      <div className="">
-        {/* <NavBar /> */}
-        <main className="p-14 w-full min-h-screen relative overflow-hidden">
-          <Hero />
-          <Feature />
-          <Collection />
-        </main>
-      </div>
+      <main className="p-14 w-full min-h-screen relative overflow-hidden">
+        <NavBar />
+        <Hero />
+        <Feature />
+        <Collection />
+        <Footer />
+      </main>
     </Fragment>
   );
 }
---
+++ src/components/Shared/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center pt-24 bg-base-300 text-base-content text-center ">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
